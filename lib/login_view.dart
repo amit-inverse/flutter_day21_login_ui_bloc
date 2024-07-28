4ea0e1cc5f30dee2b2ec376a7df0ced3d3b60374
Amit Kumar Das <amit.inverse@gmail.com>
2024-07-28 09:54:24 +0530

    Initial commit

---
+++ lib/login_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _passwordField(),
+    );
+  }
+
+  Widget _passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        icon: Icon(Icons.security),
+        hintText: 'Password',
+      ),
+    );
+  }
+}
